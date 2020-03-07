67d7dd44642f83421da8cca7e5f1a012751e5eba
Uamhan <Uamhanl@gmail.com>
2020-03-07 14:57:36 +0000

    added buyprice to values taken from website.

--- ShopSpider.py
+++ ShopSpider.py
@@ -5,7 +5,7 @@ import pyderman as dr
 #template for product object
 class Product:
     name = ""
-    buyprice = 0
+    buyprice = ""
 
 #instals chrome driver for selenium iif not present
 path = dr.install(browser=dr.chrome, file_directory='./lib/', verbose=True, chmod=True, overwrite=False, version=None, filename=None, return_info=False)
@@ -29,9 +29,14 @@ while not loaded:
 #creates product object and apends to list from items pull from website
 for item in driver.find_elements_by_class_name('desc'):
     product = Product()
-    product.name = item.find_element_by_tag_name('h1').text
+    #takes product name from h1 tag
+    product.name = item.find_element_by_tag_name('h1').tex
+    #takes price from class 'priceTxt' and takes the value after the € symbol
+    product.buyprice = item.find_element_by_class_name('priceTxt').text
+    product.buyprice = product.buyprice.partition('€')[2]
+    #adds product to product list
     products.append(product)
 #prints product names
 for item in products:
-    print(item.name)
+    print(item.name + " | " + item.buyprice)
 
